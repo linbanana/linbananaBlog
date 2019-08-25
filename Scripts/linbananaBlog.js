c37e09409b435150a327f2fc853906ca68a7724e
linbanana <tommy12345689@yahoo.com.tw>
2019-08-25 23:32:04 +0800

    test

--- Scripts/linbananaBlog.js
+++ Scripts/linbananaBlog.js
@@ -5,3 +5,11 @@
     js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
+
+/*Disqus評論*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://linbanana.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
