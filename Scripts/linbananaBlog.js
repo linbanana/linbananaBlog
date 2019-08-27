7223de3b4e81554f328d0127518c379094dd7000
linbanana <tommy12345689@yahoo.com.tw>
2019-08-27 13:51:55 +0800

    cc

--- Scripts/linbananaBlog.js
+++ Scripts/linbananaBlog.js
@@ -8,3 +8,16 @@
 }(document, 'script', 'facebook-jssdk'));
 /*facebook*/
 
+/* navbar scroll functions */
+$(window).scroll(function(e) {
+
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
+
+});
+/* navbar scroll functions */
