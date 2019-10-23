b35f9618fc5e62d2afd699b29ce46c4857e35f83
linbanana <tommy12345689@yahoo.com.tw>
2019-10-23 15:09:09 +0800

    Update linbananaBlog.js

--- Scripts/linbananaBlog.js
+++ Scripts/linbananaBlog.js
@@ -70,3 +70,24 @@ function Followfunction() {
   }
 }
 /* Follow */
+
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel();
+    
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+    
+  // Enable Carousel Controls
+  $(".carousel-control-prev").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".carousel-control-next").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
