15ceb778f2aac0cce7c6e67f5d23bf4367a53eb9
linbanana <tommy12345689@yahoo.com.tw>
2020-01-21 10:16:39 +0800

    update

--- Scripts/linbananaBlog.js
+++ Scripts/linbananaBlog.js
@@ -70,3 +70,44 @@ function Followfunction() {
   }
 }
 /* Follow */
+
+/* */
+
+$(function(){
+  var setFilter = $('#filterBtn'),
+  filterBtn = setFilter.find('a'),
+  btnAll = $('.allItem'),
+  setList = $('#filterList'),
+  filterList = setList.find('li'),
+  listWidth = filterList.outerWidth();
+
+  filterBtn.click(function(){
+    if(!($(this).hasClass('active'))){
+      var filterClass = $(this).attr('class');
+
+      filterList.each(function(){
+        if($(this).hasClass(filterClass)){
+          $(this).css({display:'block'}).stop().animate({width:listWidth},1500);
+          $(this).find('*').stop().animate({opacity:'1'},1500);
+        } else {
+          $(this).find('*').stop().animate({opacity:'0'},1000);
+          $(this).stop().animate({width:'0'},1000,function(){
+            $(this).css({display:'none'});
+          });
+        }
+      });
+      filterBtn.removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+
+  btnAll.click(function(){
+    filterList.each(function(){
+      $(this).css({display:'block'}).stop().animate({width:listWidth},1500);
+      $(this).find('*').stop().animate({opacity:'1'},1500);
+    });
+  });
+
+});
+
+/* */
