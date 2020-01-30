340e276658f2c4b053928d5c511a1afc33fa6214
linbanana <tommy12345689@yahoo.com.tw>
2020-01-30 10:50:55 +0800

    Update body.js

--- body.js
+++ body.js
@@ -1,20 +1,21 @@
-window.οnlοad=function(){function include(file) {
+window.οnlοad=function(){
+	function include(file) {
 
-  var script  = document.createElement('script');
-  script.src  = file;
-  script.type = 'text/javascript';
-  script.defer = true;
+		var script  = document.createElement('script');
+		script.src  = file;
+		script.type = 'text/javascript';
+		script.defer = true;
 
-  document.getElementsByTagName('body').item(0).appendChild(script);
+		document.getElementsByTagName('body').item(0).appendChild(script);
 
-}
+	}
 
-/* Include Many js files */
-include('\\linbananaBlog/Scripts/jquery-3.4.1.slim.min.js');
-include('\\linbananaBlog/Scripts/jquery-1.11.3.min.js');
-include('\\linbananaBlog/Scripts/popper.min.js');
-include('\\linbananaBlog/Scripts/bootstrap.min.js');
-include('\\linbananaBlog/Scripts/linbananaBlog.js');
-include('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90');
-include('//linbanana.disqus.com/count.js');
+	/* Include Many js files */
+	include('\\linbananaBlog/Scripts/jquery-3.4.1.slim.min.js');
+	include('\\linbananaBlog/Scripts/jquery-1.11.3.min.js');
+	include('\\linbananaBlog/Scripts/popper.min.js');
+	include('\\linbananaBlog/Scripts/bootstrap.min.js');
+	include('\\linbananaBlog/Scripts/linbananaBlog.js');
+	include('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90');
+	include('//linbanana.disqus.com/count.js');
 }
