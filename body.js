a44509a48c8fdf7b74904af35ec7ed540d8614c2
linbanana <tommy12345689@yahoo.com.tw>
2020-08-17 14:11:52 +0800

    Revert "Update body.js"
    This reverts commit e0c6baa4143c5b1db355beffc01a03af23f97a16.

--- body.js
+++ body.js
@@ -10,10 +10,10 @@ function include(file) {
 }
 
 /* Include Many js files */
-include('./Scripts/jquery-3.4.1.slim.min.js');
-include('./Scripts/jquery-1.11.3.min.js');
-include('./Scripts/popper.min.js');
-include('./Scripts/bootstrap.min.js');
-include('./Scripts/linbananaBlog.js');
+include('./linbananaBlog/Scripts/jquery-3.4.1.slim.min.js');
+include('./linbananaBlog/Scripts/jquery-1.11.3.min.js');
+include('./linbananaBlog/Scripts/popper.min.js');
+include('./linbananaBlog/Scripts/bootstrap.min.js');
+include('./linbananaBlog/Scripts/linbananaBlog.js');
 include('./s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90');
 include('./linbanana.disqus.com/count.js');
